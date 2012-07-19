b96e6e1d45c3cd489fcf3872064dbc8485c01932
Nicholai de Guzman <nicholai.deguzman@utsouthwestern.edu>
2012-07-19 09:33:15 -0500

    first commit, ran kitchen surveys locally

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Examsapp::Application.config.secret_token = 'cfa07f912d4faff0d9c97a6585d26076d08d4860a91d7d985b1d94fb33412bd4687e8fad4fa3adc036b7ecbc710cb3e54befbcdd874a08adba5db671f5c7d2f5'
---
+++ db/migrate/20120718222941_update_blank_versions_on_surveys.rb
@@ -0,0 +1,12 @@
+class Survey < ActiveRecord::Base; end
+class UpdateBlankVersionsOnSurveys < ActiveRecord::Migration
+  def self.up
+    Survey.where('survey_version IS ?', nil).each do |s|
+      s.survey_version = 0
+      s.save!
+    end
+  end
+
+  def self.down
+  end
+end
